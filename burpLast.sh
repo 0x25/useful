be7a74c6a82a18e481b4bf1c9d48f42c8300dc2a
john doe <0x25@users.noreply.github.com>
2017-06-19 15:17:32 +0200

    Update burpLast.sh

--- burpLast.sh
+++ burpLast.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -L 'https://portswigger.net/DownloadUpdate.ashx?Product=Free' -o burpLast.$
+curl -L 'https://portswigger.net/DownloadUpdate.ashx?Product=Free' -o burpLast.jar
 # start burp
 # java -jar -Xmx1024m burpLast.jar
 # 1024 is memory allocation
